aae8360e426be45658f5e86e7ad1ad95f17d6b39
DominicObil <145765375+DominicObil@users.noreply.github.com>
2024-03-08 10:46:07 +0100

    Add files via upload

--- Proveedor.java
+++ Proveedor.java
@@ -5,6 +5,12 @@ public class Proveedor {
 	protected String CIF;
 	protected String nombre;
 	protected int telefono;
+	public Proveedor(String cIF, String nombre, int telefono) {
+		super();
+		CIF = cIF;
+		this.nombre = nombre;
+		this.telefono = telefono;
+	}
 	
 	
 	
